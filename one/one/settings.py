e3f1f5e51c3f1d97053ef2abe63c14b1e6d01619
Mykola Rebezniuk <mkla.rbzk@gmail.com>
2023-08-22 18:38:23 -0400

    Front Page setup.

--- one/one/settings.py
+++ one/one/settings.py
@@ -179,6 +179,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
+STATICFILES_DIRS = [
+    BASE_DIR / "assets",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- one/one/urls.py
+++ one/one/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from hello.views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("polls.urls")),
-]
+    path("", include("hello.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
